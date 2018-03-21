6cb2880b9f2bada4b8c9125ef37c300acef1237e
samwhindleton <samwhindleton@gmail.com>
2018-03-21 12:45:22 -0700

    added middleware: bodyParser, static, methodOverride, session, to server.js

--- server.js
+++ server.js
@@ -7,3 +7,22 @@ const bcrypt = require('bcrypt');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const session = require('express-session');
+
+
+// ----------------------------------------
+// | MIDDLEWARE                           |
+// ----------------------------------------
+// bodyParser
+app.use(express.urlencoded({ extended: false }));
+// serve static files (css/js)
+app.use(express.static('public'));
+// method-override
+app.use(methodOverride('_method'));
+// express-session
+app.use(session(
+  {
+    secret: "pinkdotsandchapstick", // any random string
+    resave: false,
+    saveUninitialized: false
+  }
+));
