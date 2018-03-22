4eb3c2029bc5813140152b44afa5a57c0ca90334
samwhindleton <samwhindleton@gmail.com>
2018-03-22 06:34:19 -0700

    [1] created controller/register.js, added dependencies, models, post route for create new user, module exports.

---
+++ controllers/register.js
@@ -0,0 +1,54 @@
+// ----------------------------------------
+// | DEPENDENCIES                         |
+// ----------------------------------------
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+
+// ----------------------------------------
+// | MODELS                               |
+// ----------------------------------------
+const User = require('../models/users.js');
+
+
+// ----------------------------------------
+// | ROUTES                               |
+// ----------------------------------------
+// --------------------                   |
+// 7 Restful Routes   |                   |
+// --------------------                   |
+// Index  : GET    '/'                1/7 |
+// Show   : GET    '/:id'             2/7 |
+// New    : GET    '/new'             3/7 |
+// Create : POST   '/'                4/7 |
+// Edit   : GET    '/:id/edit'        5/7 |
+// Update : PUT    '/:id'             6/7 |
+// Delete : DELETE '/:id'             7/7 |
+// ----------------------------------------
+
+// Create : POST   '/'                4/7 |
+// Create new user
+router.post('/', (req, res) => {
+  // res.send(req.body);
+
+  // Encrypt password on Create/register new user
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+
+  User.create(req.body, (error, user) => {
+    if (error) {
+      res.send (error);
+    } else {
+      // res.send('Welcome ' + user.username + '!');
+      res.render('./user/index.ejs', {user:user});
+    }
+  });
+});
+
+// ----------------------------------------
+// | MODULE EXPORTS                       |
+// ----------------------------------------
+// Access this file in server.js          |
+// Export router & require in server.js   |
+// ----------------------------------------
+module.exports = router;
