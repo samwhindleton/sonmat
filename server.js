af97d40c6d1d1296b7135da6b5498aca882ade24
samwhindleton <samwhindleton@gmail.com>
2018-03-22 08:13:40 -0700

    updating package.json and updating code for heroku

--- server.js
+++ server.js
@@ -86,6 +86,7 @@ mongoose.connection.once('open', () => {
 // ----------------------------------------
 // | LISTENER                             |
 // ----------------------------------------
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('Now Listening on Port 3000');
 });
